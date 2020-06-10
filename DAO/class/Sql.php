dc51f9e54981df9a48e65515ee66586423bc6993
Victor Costa <victor99costa@outlook.com>
2020-06-10 16:39:22 -0300

    criei a classe Usuario

--- DAO/class/Sql.php
+++ DAO/class/Sql.php
@@ -11,20 +11,21 @@ class Sql extends PDO {
     }
 
     
-    private function setParam($statement, $key, $value) {
-        
-        $statement->bindParam($key, $value);
-        
-    }
     
     private function setParams($statement, $parameters = array()) {
-
+        
         foreach ($parameters as $key => $value) {
-
-            $this->setParam($key, $value);
-
+            
+            $this->setParam($statement, $key, $value);
+            
         }
+        
+    }
 
+    private function setParam($statement, $key, $value) {
+                
+        $statement->bindParam($key, $value);
+                
     }
     public function query($rawQuery, $params = array()) {
 
--- DAO/index.php
+++ DAO/index.php
@@ -2,10 +2,13 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+$root = new Usuario();
 
+$root->loadbyId(4);
+
+echo $root;
+
+/*$sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-echo json_encode($usuarios);
-
+echo json_encode($usuarios);*/
 ?>
