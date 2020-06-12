0a9f2009109ea60c3f7ac994db2b2406a69ffce6
Victor Costa <victor99costa@outlook.com>
2020-06-12 09:02:31 -0300

    adicionei os metodos de lista

--- DAO/index.php
+++ DAO/index.php
@@ -2,13 +2,27 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-
-$root->loadbyId(4);
-
-echo $root;
-
 /*$sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);*/
+
+//Carrega um so usuario
+/*$root = new Usuario();
+$root->loadbyId(4);
+echo $root;*/
+
+//Carrega uma lista de usuarios
+/*$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+//Carrega uma lista de usuarios pelo login
+//$search = Usuario::search("r");
+//echo json_encode($search);
+
+//Carrega um usuario autenticando antes o acesso
+$usuario = new Usuario();
+$usuario->login("richard", "50135");
+
+echo $usuario;
+
 ?>
